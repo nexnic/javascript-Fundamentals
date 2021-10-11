19221eb6443f2fb17e7f9899825e4ac4ba307397
kent erik Hole <kent.erik.hole@outlook.com>
2021-10-11 23:13:06 +0200

    kurs

---
+++ 01-Fundamentals-Part-1/lecture/part 1/lecture1/script.js
@@ -0,0 +1,9 @@
+const country = "Norway";
+const continent = "EU";
+let population = 5391369;
+let isIsland = false;
+let language = "Norsk";
+
+
+console.log(isIsland, population, country, language);
+
